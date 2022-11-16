ac7ec2c4e3c1cd4dbfecafabb264cd5625ff20fc
Thamico <thanh.nguyenctn1999@gmail.com>
2022-11-16 19:33:10 +0100

    addBookToLibrary() implemented

--- library.js
+++ library.js
@@ -1,9 +1,19 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(name, author, pages, read) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary(book) {
+function addBookToLibrary() {
+  const book = new Book(
+    document.getElementById("bname").value,
+    document.getElementById("aname").value,
+    document.getElementById("pages").value,
+    document.getElementById("scales").value
+  );
   myLibrary.push(book);
+  console.log(book);
 }
